ac7466bd0dbe1b0c8b6b035a9b798de77df65401
Pragya <pragyatripathy3012@gmail.com>
2022-10-05 14:28:10 +0200

    good

--- html/search.js
+++ html/search.js
@@ -1,5 +1,10 @@
 const inputElement = document.querySelector('#input');
 const btnElement = document.querySelector('#submit');
+const imgsElement = document.querySelectorAll('.poster1');
+const h4Element = document.querySelectorAll('.title');
+const searchDuration = document.querySelectorAll('.searchduration');
+const searchScore = document.querySelectorAll('.searchscore');
+
 
 fetch('https://api.jikan.moe/v4/anime?q='+inputElement.value)
 .then((response)=> {
@@ -8,9 +13,36 @@ fetch('https://api.jikan.moe/v4/anime?q='+inputElement.value)
 })
 .then ((data)=>{
     console.log(data);
-})
+    for(let index=0;index<4;index++){
+        console.log(data[index]);
+        imgsElement[index].src=data.data[index].images.jpg.image_url
+        h4Element[index].textContent= data.data[index].title
+        searchDuration[index].textContent=data.data[index].duration
+        searchScore[index].textContent =data.data[index].score
 
-inputElement.addEventListener('click',function(){
+    }
+});
+
+btnElement.addEventListener('click',function(){
+    fetch('https://api.jikan.moe/v4/anime?q='+inputElement.value)
+.then((response)=> {
+    console.log(response);
+    return response.json();
+})
+.then ((data)=>{
+    console.log(data);
+    for(let index=0;index<4;index++){
+        console.log(data[index]);
+        imgsElement[index].src=data.data[index].images.jpg.image_url
+        h4Element[index].textContent= data.data[index].title
+        searchDuration[index].textContent=data.data[index].duration
+        searchScore[index].textContent =data.data[index].score
+
+    }
+});
+
+
+
     
 
 })
